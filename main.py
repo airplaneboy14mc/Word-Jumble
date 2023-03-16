4e02bdd6dbd0b84d3c8879aec87201ea76c4716e
Airplaneboy14 <84548624+airplaneboy14mc@users.noreply.github.com>
2023-03-16 13:32:02 -0500

    Update main.py
    Signed-off-by: Airplaneboy14 <84548624+airplaneboy14mc@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import random
 import os
 import time
 
+
 #define main function
 def main():
 	try:
 		#tell user about the game
 		print("Welcome to Word Jumble!")
-		print("This will select a random word from a predefined list of words and jumble them up.\nYou will then have to guess the word based off of the jumbled letters.")
+		print(
+		 "This will select a random word from a predefined list of words and jumble them up.\nYou will then have to guess the word based off of the jumbled letters."
+		)
 		#create tuple
 		words = init_tuple()
 		#choose the word
@@ -27,7 +30,7 @@ def main():
 		while rand_word:
 			position = random.randrange(len(rand_word))
 			jumble += rand_word[position]
-			rand_word = rand_word[:position]+rand_word[(position+1):]
+			rand_word = rand_word[:position] + rand_word[(position + 1):]
 		print(f"The jumble is {jumble}.")
 		#call guess function
 		guess(correct, rand_word)
@@ -41,14 +44,24 @@ def main():
 		print()
 		main()
 
+
 #define init_tuple function
 def init_tuple():
-	deserts = ('Chocolate', 'Oreos', 'Cannoli', 'Carrot Cake', 'Swedish Fish')
-	meats = ('Chicken', 'Beef', 'Turkey', 'Pork')
-	fruits = ('Banana', 'Dragon Fruit', 'Orange', 'Kiwi', 'Apple')
-	vegetables = ('Cucumber', 'Bell Pepper', 'Carrot', 'Romain Lettuce', 'Spinach', 'Kale', 'Celery')
+	deserts = ('Chocolate', 'Oreos', 'Cannoli', 'Carrot Cake', 'Swedish Fish',
+	           'Pecan Pie', 'Cheesecake', 'Creme Brulee', 'Black Forest Gateau',
+	           'Gelato', 'Ice Cream', 'Pumpkin Pie', 'Cherry Pie')
+	meats = ('Chicken', 'Beef', 'Turkey', 'Pork', 'Ham', 'Venison', 'Buffalo',
+	         'Salmon', 'Goose', 'Boar', 'Tuna', 'Crab', 'Bison', 'Calamari',
+	         'Octopus')
+	fruits = ('Banana', 'Dragon Fruit', 'Orange', 'Kiwi', 'Apple', 'Mango',
+	          'Pineapple', 'Nectarine', 'Raspberry', 'Strawberry', 'Cherry',
+	          'Grapefruit', 'Star Fruit', 'Pear', 'Lemon', 'Blueberry', 'Date',
+	          'Honeydew', 'Tomato', 'Passion Fruit')
+	vegetables = ('Cucumber', 'Bell Pepper', 'Carrot', 'Romain Lettuce',
+	              'Spinach', 'Kale', 'Celery')
 	categories = (deserts, meats, fruits, vegetables)
 	return categories
+
 
 #define check_cat function
 def check_cat(category):
@@ -64,6 +77,7 @@ def check_cat(category):
 	else:
 		return "Could not determine category."
 
+
 #define guess function
 def guess(correct, word):
 	#get the user's guess
@@ -76,9 +90,11 @@ def guess(correct, word):
 		else:
 			print("\nYou guessed correctly!\n")
 
+
 #function to clear the screen
 def clear():
 	os.system('clear')
+
 
 #function to control running again
 def run_again():
@@ -96,10 +112,12 @@ def run_again():
 		clear()
 		KeyboardInterrupt
 
+
 class error_color:
 	red = '\u001b[31m'
 	warn = '\033[93m'
 	endc = '\033[0m'
-	
+
+
 #call main
 main()
