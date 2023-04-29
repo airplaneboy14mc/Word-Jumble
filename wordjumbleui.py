73a3f046356377ee74930c11f827c98232aadc36
Airplaneboy14 <84548624+airplaneboy14mc@users.noreply.github.com>
2023-04-29 08:25:22 -0500

    Upload the version used in 23w17a
    Signed-off-by: Airplaneboy14 <84548624+airplaneboy14mc@users.noreply.github.com>

--- wordjumbleui.py
+++ wordjumbleui.py
@@ -33,58 +33,84 @@ def check_cat(category):
 	else:
 		return "Could not determine category."
 def main():
-    words = init_tuple()
-    rand_cat = random.choice(words)
-    rand_word = random.choice(rand_cat)
-    category = check_cat(rand_cat)
-    correct = rand_word
-    jumble = ""
-    while rand_word:
-        position = random.randrange(len(rand_word))
-        jumble += rand_word[position]
-        rand_word = rand_word[:position] + rand_word[(position + 1):]
+    try:
+        def quit():
+            main_window.destroy()
+        
+        words = init_tuple()
+        rand_cat = random.choice(words)
+        rand_word = random.choice(rand_cat)
+        category = check_cat(rand_cat)
+        correct = rand_word
+        jumble = ""
+        while rand_word:
+            position = random.randrange(len(rand_word))
+            jumble += rand_word[position]
+            rand_word = rand_word[:position] + rand_word[(position + 1):]
+            if jumble == correct:
+                quit()
+                main()
+            else:
+                pass
 
-    def guess(correct, word):
-        guess = str(entry.get())
-        if correct != guess:
-            messagebox.showerror('Incorrect', 'The word is not correct')
-        else:
-            messagebox.showinfo('Correct', 'The word is correct!')
+        def guess():
+            guess = str(entry.get())
+            if correct != guess:
+                messagebox.showerror('Incorrect', 'The word is not correct.')
+            else:
+                messagebox.showinfo('Correct', 'The word is correct!')
+                quit()
+                main()
+        
+        def handler(e):
+            guess = str(entry.get())
+            if correct != guess:
+                messagebox.showerror('Incorrect', 'The word is not correct.')
+            else:
+                messagebox.showinfo('Correct', 'The word is correct!')
+                quit()
+                main()
+        
+        def exit_program(e):
             quit()
-            main()
-        
-    def quit():
-        main_window.destroy()
 
-    main_window = tk.Tk()
-    main_window.title('Word Jumble Game')
-    main_window.geometry('420x275')
-    main_window.resizable(False, False)
-        
-    top_frame = tk.Frame(main_window)
-    bottom_frame = tk.Frame(main_window)
-        
-    top_frame.pack(side='top')
-    bottom_frame.pack(side='bottom')
-        
-    top_label = tk.Label(top_frame, text='Word Jumble Game', font=('Comic Sans MS', 38))
-    top_label.pack(side='top')
-    enter_guess = tk.Label(bottom_frame, text='Enter your guess:', font=('Comic Sans MS', 16))
-    enter_guess.pack(side='top', padx=10)
-    entry = tk.Entry(bottom_frame)
-    entry.pack(side='top')
-    cat_label = tk.Label(top_frame, text=f'Category: {category}', font=('Comic Sans MS', 22))
-    cat_label.pack(side='top')
-    jumble_label = tk.Label(top_frame, text=f'Jumble: {jumble}', font=('Comic Sans MS', 22))
-    jumble_label.pack(side='top')
+        main_window = tk.Tk()
+        main_window.title('Word Jumble Game')
+        main_window.geometry('420x275')
+        main_window.resizable(False, False)
+        main_window.bind('<Return>', handler)
+        main_window.bind('<Escape>', exit_program)
+        main_window.bind('<Delete>', exit_program)
+        #Working on Icon stuff, might take a while however
+        #main_window.iconphoto(False, 'wordjumbleicon.png')
+            
+        top_frame = tk.Frame(main_window)
+        bottom_frame = tk.Frame(main_window)
+            
+        top_frame.pack(side='top')
+        bottom_frame.pack(side='bottom')
+            
+        top_label = tk.Label(top_frame, text='Word Jumble Game', font=('Comic Sans MS', 38))
+        top_label.pack(side='top')
+        enter_guess = tk.Label(bottom_frame, text='Enter your guess:', font=('Comic Sans MS', 16))
+        enter_guess.pack(side='top', padx=10)
+        entry = tk.Entry(bottom_frame)
+        entry.pack(side='top')
+        cat_label = tk.Label(top_frame, text=f'Category: {category}', font=('Comic Sans MS', 22))
+        cat_label.pack(side='top')
+        jumble_label = tk.Label(top_frame, text=f'Jumble: {jumble}', font=('Comic Sans MS', 22))
+        jumble_label.pack(side='top')
 
-    enter_button = tk.Button(bottom_frame, text='Enter', font=('Comic Sans MS', 16), command=lambda: guess(correct, rand_word))
-    enter_button.pack(side='left', pady=5, padx=3)
-    quit_button = tk.Button(bottom_frame, text='Quit', font=('Comic Sans MS', 16), command=quit)
-    quit_button.pack(side='left', pady=5, padx=3)
-    about_button = tk.Button(bottom_frame, text='About', font=('Comic Sans MS', 16), command=lambda: messagebox.showinfo('About','This game chooses a word from a category, then jumbles the letters and lets the user guess the word'))
-    about_button.pack(side='left', pady=5, padx=3)
+        enter_button = tk.Button(bottom_frame, text='Enter', font=('Comic Sans MS', 16), command=guess)
+        enter_button.pack(side='left', pady=5, padx=3)
+        quit_button = tk.Button(bottom_frame, text='Quit', font=('Comic Sans MS', 16), command=quit)
+        quit_button.pack(side='left', pady=5, padx=3)
+        about_button = tk.Button(bottom_frame, text='About', font=('Comic Sans MS', 16), command=lambda: messagebox.showinfo('About','This game chooses a word from a category, then jumbles the letters and lets the user guess the word'))
+        about_button.pack(side='left', pady=5, padx=3)
+        
 
-    main_window.mainloop()
+        main_window.mainloop()
+    except NameError:
+        quit()
 
 main()
